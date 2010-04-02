c33d6a765c3de3f53ee23e7c9bb4466cca37bc94
Nikolay Matveev <nnmatveev2@gmail.com>
2010-04-02 13:10:17 +0400

    Forgot to include this sources into git

---
+++ src/protobuf/lang/psi/api/references/PbOptionRef.java
@@ -0,0 +1,10 @@
+package protobuf.lang.psi.api.references;
+
+import protobuf.lang.psi.api.PbPsiElement;
+
+/**
+ * author: Nikolay Matveev
+ * Date: Apr 1, 2010
+ */
+public interface PbOptionRef extends PbRef, PbPsiElement {
+}
