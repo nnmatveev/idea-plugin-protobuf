86adec59635408ee9f4178cf5223fbc380a14d05
Nikolay Matveev <nnmatveev@gmail.com>
2011-02-24 11:47:58 +0300

    Larger-scale testing revealed one more access of the Psi element tree that needed to be surrounded with a read/Compute block.(cherry picked from commit ff7760b973830d8d4add5c8b58ca856f3b594055)

--- src/protobuf/lang/psi/impl/declaration/PbPackageDefImpl.java
+++ src/protobuf/lang/psi/impl/declaration/PbPackageDefImpl.java
@@ -1,6 +1,8 @@
 package protobuf.lang.psi.impl.declaration;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.util.Computable;
 import protobuf.lang.psi.api.declaration.PbPackageDef;
 import protobuf.lang.psi.api.reference.PbRef;
 import protobuf.lang.psi.impl.PbPsiElementImpl;
@@ -10,13 +12,20 @@ import protobuf.lang.psi.impl.PbPsiElementImpl;
  * Date: Mar 10, 2010
  */
 public class PbPackageDefImpl extends PbPsiElementImpl implements PbPackageDef {
+    
     public PbPackageDefImpl(ASTNode node){
         super(node);
     }
 
+    // Note that this method gets its information from inspecting the Psi element tree, so must use the runReadAction and
+    // Computable because it's not run from the normal thread that deals with them.
     @Override
     public String getPackageName() {
-        final PbRef packageRef = findChildByClass(PbRef.class);
+        final PbRef packageRef = ApplicationManager.getApplication().runReadAction(new Computable<PbRef>() {
+            public PbRef compute() {
+                return findChildByClass(PbRef.class);
+            }
+        });
         if(packageRef == null){
             return "";
         }        
