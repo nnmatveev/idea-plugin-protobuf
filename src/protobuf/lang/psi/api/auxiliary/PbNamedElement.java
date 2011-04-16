493a4060bd06bd0781f701821a681979565a9530
Nikolay Matveev <nnmatveev@gmail.com>
2011-04-16 17:41:50 +0400

    minor

--- src/protobuf/lang/psi/api/auxiliary/PbNamedElement.java
+++ src/protobuf/lang/psi/api/auxiliary/PbNamedElement.java
@@ -2,11 +2,14 @@ package protobuf.lang.psi.api.auxiliary;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNamedElement;
+import protobuf.lang.psi.api.PbPsiElement;
 
 /**
  * @author Nikolay Matveev
  */
 
-public interface PbNamedElement extends PsiNamedElement {
+public interface PbNamedElement extends PbPsiElement, PsiNamedElement {
+
     PsiElement getNameElement();
+
 }
